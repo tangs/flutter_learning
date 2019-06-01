ff674e5f99f45849c30fc03aaf61b30448e5c65c
tangs <tangsong1988kaka@gmail.com>
2019-06-01 10:48:03 +0800

    udpate.

--- prj1/lib/func_mask/mask.dart
+++ prj1/lib/func_mask/mask.dart
@@ -4,17 +4,23 @@ import 'dart:io';
 import 'package:crypto/crypto.dart';
 import 'package:flutter/material.dart';
 
+import '../utils/widget_tools.dart';
+
 class _MaskUIState extends State<MaskUI> {
   Widget _buildItem(BuildContext context, List data) {
+    final size = MediaQuery.of(context).size;
+    final int cols = size.width ~/ 200;
     return GridView.count(
-      crossAxisCount: 2,
+      crossAxisCount: cols,
+      childAspectRatio: 2.5,
       children: List.generate(data.length, (index) {
         return Row(
+          // child: Text(data[index]['show']),
           children: <Widget>[
             Checkbox(
-              value: false,
+              value: true,
               onChanged: (isSelected) {
-
+                
               },
             ),
             Text(data[index]['show']),
@@ -27,8 +33,12 @@ class _MaskUIState extends State<MaskUI> {
   @override
   Widget build(BuildContext context) {
     return new FutureBuilder(
-      future: DefaultAssetBundle.of(context).loadString('assets/1.json'),
+      future: DefaultAssetBundle.of(context).loadString('assets/config.json'),
       builder: (context, snapshot) {
+        if (!snapshot.hasData) {
+          return  Scaffold();
+        }
+        final jsonData = JsonDecoder().convert(snapshot.data);
         return Scaffold(
           resizeToAvoidBottomInset: false,
           appBar: new AppBar(
@@ -37,13 +47,35 @@ class _MaskUIState extends State<MaskUI> {
           body: Builder(
             builder: (context) => Container(
               padding: EdgeInsets.all(32),
+              // child: _buildItem(context, jsonData['pay']),
               child: Column(
                 // mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: <Widget>[
-                  // _buildItem(context, snapshot.data['pay']),
-                  // _buildItem(context, snapshot.data['hall']),
+                  Row(
+                    children: <Widget>[
+                      Text('Pay'),
+                    ]
+                  ),
+                  Expanded(
+                    child: _buildItem(context, jsonData['pay'])
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Text('hall'),
+                    ]
+                  ),
+                  Expanded(
+                    child: _buildItem(context, jsonData['hall'])
+                  ),
+                  WidgetTools.buildTextField('version', TextInputType.text, (String text) {
+                    // username = text;
+                  }),
+                  WidgetTools.buildTextField('chanels', TextInputType.text, (String text) {
+                    // username = text;
+                  }),
+                  // _buildItem(context, jsonData['hall']),
                 ],
-            ),
+              ),
             ),
           ),
         );
